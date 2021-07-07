b0bec786548f968dc353d9a2141391cd83892aac
Choi Juhwan <gold32317@gmail.com>
2021-07-08 00:08:02 +0900

    File for download COCO 2017 dataset

---
+++ download.sh
@@ -0,0 +1,14 @@
+mkdir dataset
+wget http://images.cocodataset.org/annotations/annotations_trainval2017.zip -P ./dataset/
+wget http://images.cocodataset.org/zips/train2017.zip -P ./dataset/
+wget http://images.cocodataset.org/zips/val2017.zip -P ./dataset/
+wget http://images.cocodataset.org/zips/test2017.zip -P ./dataset/
+
+unzip ./dataset/train2017.zip -d ./dataset/
+rm ./dataset/train2017.zip
+unzip ./dataset/val2017.zip -d ./dataset/
+rm ./dataset/val2017.zip
+unzip ./dataset/test2017.zip -d ./dataset/
+rm ./dataset/test2017.zip
+unzip ./dataset/annotations_trainval2017.zip -d ./dataset/
+rm ./dataset/annotations_trainval2017.zip
