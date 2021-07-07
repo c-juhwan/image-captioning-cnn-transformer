472745f918107d0a8fde66f990074d54f144ea5f
Choi Juhwan <gold32317@gmail.com>
2021-07-08 00:38:35 +0900

    Add new sh file for download requirements

---
+++ download_requirements.sh
@@ -0,0 +1,11 @@
+# Install pip packages
+pip install -r requirements.txt
+
+# Install COCO API
+git clone https://github.com/pdollar/coco.git
+cd coco/PythonAPI/
+make
+python setup.py build
+python setup.py install
+
+cd ../../
