5422f94820fe0f989f9e2e082a272922b2b49f97
Choi Juhwan <gold32317@gmail.com>
2021-07-09 02:47:48 +0900

    added rm

--- preprocessing.sh
+++ preprocessing.sh
@@ -4,3 +4,7 @@ python build_vocab.py --caption_path="dataset/annotations/captions_val2017.json"
 python resize_image.py --image_dir="./dataset/train2017/" --output_dir="./dataset/resized_train2017/"
 python resize_image.py --image_dir="./dataset/test2017/" --output_dir="./dataset/resized_test2017/"
 python resize_image.py --image_dir="./dataset/val2017/" --output_dir="./dataset/resized_val2017/"
+
+rm ./dataset/train2017/
+rm ./dataset/test017/
+rm ./dataset/val2017/
