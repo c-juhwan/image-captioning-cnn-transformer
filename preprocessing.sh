6321b284a6012a72cea0955d654520448b0977fc
Choi Juhwan <gold32317@gmail.com>
2021-07-10 20:37:20 +0900

    removed resize_image.py

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,9 +1,1 @@
 python build_vocab.py
-
-python resize_image.py --image_dir="./dataset/train2017/" --output_dir="./dataset/resized_train2017/"
-python resize_image.py --image_dir="./dataset/test2017/" --output_dir="./dataset/resized_test2017/"
-python resize_image.py --image_dir="./dataset/val2017/" --output_dir="./dataset/resized_val2017/"
-
-rm -rd ./dataset/train2017/
-rm -rd ./dataset/test2017/
-rm -rd ./dataset/val2017/
