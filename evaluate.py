6aea9d5232cab5f4021d48591de98c0b679db919
Choi Juhwan <gold32317@gmail.com>
2021-07-12 01:39:44 +0900

    commit

--- evaluate.py
+++ evaluate.py
@@ -4,56 +4,35 @@ import torch.nn as nn
 import numpy as np
 import os
 import pickle
-import nltk
 from data_loader import get_loader 
-from build_vocab import Vocabulary
-from model import Encoder, Decoder
+from model import CaptioningModel
 from torchvision import transforms
 from tqdm.auto import tqdm
-import nltk.translate.bleu_score as bleu
+from torchtext.vocab import Vocab
 
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-def main():
-    # bleu.corpus_bleu()
-
+def main(args):
     transform = transforms.Compose([ 
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(), 
+        transforms.Resize((args.resize_size, args.resize_size)),
+        transforms.ToTensor(),
         transforms.Normalize((0.485, 0.456, 0.406),
-                             (0.229, 0.224, 0.225))])
-
-    with open(args.vocab_path, 'rb') as f:
-        vocab = pickle.load(f)
-
-    # Build models
-    encoder = Encoder(args.embed_size).eval()  # eval mode (batchnorm uses moving mean/variance)
-    decoder = Decoder(args.embed_size, args.hidden_size, len(vocab), args.num_layers)
-    encoder = encoder.to(device)
-    decoder = decoder.to(device)
-
-    # Load the trained model parameters
-    encoder.load_state_dict(torch.load(args.encoder_path))
-    decoder.load_state_dict(torch.load(args.decoder_path))
-
-    data_loader_test = get_loader(args.image_dir_test, args.caption_path_test, vocab, 
-                             transform, args.batch_size,
-                             shuffle=True, num_workers=args.num_workers) 
+                             (0.229, 0.224, 0.225))])    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--encoder_path', type=str, default='models/encoder-5-3000.ckpt', help='path for trained encoder')
-    parser.add_argument('--decoder_path', type=str, default='models/decoder-5-3000.ckpt', help='path for trained decoder')
+    parser.add_argument('--model_path', type=str, default='models/' , help='path for loading trained models')
+    parser.add_argument('--resize_size', type=int, default=224 , help='size for resizing images')
+    parser.add_argument('--max_len', type=int, default=300 , help='maximum length for each caption')
     parser.add_argument('--vocab_path', type=str, default='dataset/vocab.pkl', help='path for vocabulary wrapper')
-    parser.add_argument('--image_dir_test', type=str, default='dataset/resized_val2017', help='directory for resized test images')
-    parser.add_argument('--caption_path_test', type=str, default='dataset/annotations/captions_train2017.json', help='path for test annotation json file')
-    
+    parser.add_argument('--image_dir_test', type=str, default='dataset/test2017', help='directory for resized validation images')
+
     # Model parameters
-    parser.add_argument('--embed_size', type=int , default=512, help='dimension of word embedding vectors')
+    parser.add_argument('--embed_size', type=int , default=512, help='dimension of word embedding vectors, using Glove dim=300')
     
-    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--num_epochs', type=int, default=1)
+    parser.add_argument('--batch_size', type=int, default=64)
     parser.add_argument('--num_workers', type=int, default=2)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
     args = parser.parse_args()
     print(args)
     main(args)
--- model.py
+++ model.py
@@ -1,41 +1,58 @@
 import torch
 import torch.nn as nn
-import numpy as np
 import math
 from efficientnet_pytorch import EfficientNet
 
 
 class CaptioningModel(nn.Module):
-    def __init__(self, batch_size, embed_size, vocab_size):
+    def __init__(self, batch_size, vocab_size, embed_size=256, d_model=512, max_len=300):
+        """
+        Load pretrained EfficientNet-b7 and initialize other layers
+        """
         super(CaptioningModel, self).__init__()
         self.batch_size = batch_size
         self.feature_size = self.efficientnet._fc.in_features
 
         self.efficientnet = EfficientNet.from_pretrained('efficientnet-b7')
         self.pixel_embed = PixelEmbedding()
-        self.linear_feature = nn.Linear(self.feature_size, embed_size)
-        self.text_embed = TextEmbedding(vocab_size, embed_size, padding_idx=0)
-        self.transformer = nn.Transformer(d_model=embed_size, batch_first=True)
-        self.linear_out = nn.Linear(embed_size, vocab_size)
+        self.linear_feature = nn.Linear(self.feature_size, d_model)
+        self.text_embed = TextEmbedding(vocab_size, embed_size, d_model, max_len, padding_idx=0)
+        self.transformer = nn.Transformer(d_model=d_model, batch_first=True)
+        self.linear_out = nn.Linear(d_model, vocab_size) 
 
-    def forward(self, images, captions):
+        self.efficientnet.requires_grad_(False)
+    
+    def forward(self, images, captions, non_pad_pos):
+        """ 
+        Args: http
+
+        Return:
+            torch.Tensor with size (batch_size, max_len, vocab_size)
+            word estimation
+        """
+        # images: (batch_size, 3, H, W) -> default HxW = 224x224 
+        # captions: (batch_size, max_len) -> default max_len is 300, caption[1] is vocabulary index
         with torch.no_grad():
+            # Extract feature from EfficientNet
             features = self.efficientnet.extract_features(images)
-        features = features.view(self.batch_size, self.feature_size, -1)
-        features = features.transpose(1, 2) # batch_size, 49, 2560
+            # features: (batch_size, 2560, H', W') -> H'xW' = 7x7 in case of 224x224
+        
+        features = features.view(features.size(0), features.size(1), -1) # features: (batch_size, 2560, 49)
+        features = features.transpose(1, 2) # features: (batch_size, 49, 2560)
+        features = self.pixel_embed(features) # features: (batch_size, 50, 2560)
+        features = self.linear_feature(features) # features: (batch_size, 50, d_model)
 
-        features = self.pixel_embed(features)
+        embedding = self.text_embed(captions) # embedding: (batch_size, max_len, d_model)
 
-        features = self.linear_feature(features)
+        out = self.transformer(src=features, tgt=embedding)
+        # nn.Trasformer
+        # src=features: (batch_size, 50, 512) -> N=batch_size, S=50=Source Seq Length, E=d_model=feature number
+        # tgt=embedding: (batch_size, 300, 512) -> N=batch_size, T=max_len=Target Seq Length, E=d_model=feature number
+        # out: (batch_size, 300, 512) -> N, T, E
+        out = out[non_pad_pos] # delete padding part
+        out = self.linear_out(out) # out: (batch_size, len, vocab_size)
 
-        text_embedding = self.text_embed(captions)
-
-        out = self.transformer(src=features, tgt=text_embedding)
-        out = self.linear_out(out)
-
-        return out
-
-
+        
 class PixelEmbedding(nn.Module):
     """
     Gives spatial information to each pixel extracted by cnn backbone
@@ -45,30 +62,33 @@ class PixelEmbedding(nn.Module):
     
     def forward(self, features):
         batch_size = features.size(0)
-        pixel_size = features.size(1)
-        feature_size = features.size(2)
+        pixel_num = features.size(1) # 7x7 -> 49
+        feature_num = features.size(2) # 2560
 
-        cls_token = nn.Parameter(torch.randn(1, feature_size))
-        positions = nn.Parameter(torch.randn(pixel_size + 1, feature_size))
+        cls_token = nn.Parameter(torch.randn(1, feature_num))
+        positions = nn.Parameter(torch.randn(pixel_num + 1, feature_num)) # +1 for cls_token
+        
+        cls_tokens = cls_token.repeat(batch_size, 1, 1) # cls_token: (batch_size, 1, feature_num)
 
-        cls_tokens = cls_token.repeat(batch_size, 1, 1)
-
-        out = torch.cat([cls_tokens, features], dim=1)
-        out += positions
+        out = torch.cat([cls_tokens, features], dim=1) # out: (batch_size, 1+49, feature_num)
+        out += positions # out: (batch_size, 50, feature_num) -> Broadcasting
 
         return out
 
 class TextEmbedding(nn.Module):
-    def __init__(self, vocab_size, embed_size, padding_idx):
+    def __init__(self, vocab_size, embed_size, d_model, max_len, padding_idx=0):
         super(TextEmbedding, self).__init__()
         
-        self.embed = nn.Embedding(vocab_size, embed_size, padding_idx=0)
-        self.pos_encoding = PositionalEncoding(embed_size)
-
+        self.embed = nn.Embedding(vocab_size, embed_size, padding_idx)
+        self.linear = nn.Linear(embed_size, d_model)
+        self.pos_encoding = PositionalEncoding(d_model, max_len)
 
     def forward(self, sentence):
-        embedding = self.embed(sentence)
-        pos = self.pos_encoding(sentence)
+        # sentence: (batch_size, max_len)
+        embedding = self.embed(sentence) # embedding: (batch_size, max_len, embed_size)
+        embedding = self.linear(embedding) # embedding: (batch_size, max_len, d_model)
+
+        pos = self.pos_encoding(sentence) # pos: (max_len, d_model)
 
         return embedding + pos
 
@@ -77,7 +97,7 @@ class PositionalEncoding(nn.Module):
     Reference: http://incredible.ai/nlp/2020/02/29/Transformer/#232-github에-구현한-코드-pytorch-문서-참고
     """
 
-    def __init__(self, embed_dim, dropout=0.1, max_seq_len=400):
+    def __init__(self, embed_dim, dropout=0.1, max_seq_len=300):
         super(PositionalEncoding, self).__init__()
         self.dropout = nn.Dropout(p=dropout)
 
